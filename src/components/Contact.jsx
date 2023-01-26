db5831d0c53ca3721281cfadd745fa5488857447
Rochi Albani <rocio.albani@hotmail.com>
2023-01-26 20:42:17 -0300

    fixed component contact bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { motion } from 'framer-motion';
-import { transition1 } from '../data';
 import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import {AiOutlineMail} from 'react-icons/ai';
@@ -9,13 +7,12 @@ const Contact = () => {
     return (
         <div id='contact' className="w-full h-screen text-center">
             <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-                <div className="col-span-2  bottom-0 left-0 right-0 top-72 -z-10 ">
+                <div className="col-span-2  bottom-0 left-0 right-0 top-72  ">
                     <h1 className=" py-4 font-[Raleway]  leading-[120%] tracking-[-0.05em] mb-2">Contact me</h1>
-                    
                     <form className='flex flex-col gap-y-4'>
                         <div className='flex gap-x-10'>
                             <input
-                            className='outline-none border-b  border-b-black h-[100px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
+                            className=' outline-none border-b  border-b-black h-[100px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
                             type='text'
                             placeholder='Your name'
                             />
